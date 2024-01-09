1188f512e9bdd3cb4b60e3a8369b47c173afe70e
tanmay958 <studytime958@gmail.com>
2024-01-10 03:53:49 +0530

    added some dist modification

--- dist/app/index.js
+++ dist/app/index.js
@@ -25,6 +25,9 @@ function initGraphQl() {
         const app = (0, express_1.default)();
         app.use(body_parser_1.default.json());
         app.use((0, cors_1.default)());
+        app.get("/", (req, res) => {
+            res.status(200).json({ message: "healthy" });
+        });
         const server = new server_1.ApolloServer({
             typeDefs: `
       ${User_1.User.types}
